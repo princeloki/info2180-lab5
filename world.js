fa28d6807493f2a1d520bd12189cb0eaf90fd9e1
Teriq Strachan <teriqstrachan@yahoo.com>
2020-11-19 18:39:31 -0500

    Finished main search functionality

--- world.js
+++ world.js
@@ -1,19 +1,25 @@
 window.onload = function(){
 
   var input = document.querySelector("#country");
-  var button = document.querySelector("#lookup");
+  var countryBut = document.querySelector("#lookup");
+  var citiesBut = document.querySelector("#cities")
   var result = document.querySelector("#result");
 
-  button.addEventListener("click", function(e){
+  countryBut.addEventListener("click", function(e){
     e.preventDefault();
 
     result.innerHTML = "";
-    const formData = new FormData();
-    formData.append("country",input.value);
-    fetchData(input.value);
+    fetchCountry(input.value);
   });
 
-  async function fetchData(val){
+  citiesBut.addEventListener("click", function(e){
+    e.preventDefault();
+    result.innerHTML = "";
+
+    fetchCity(input.value);
+  });
+
+  async function fetchCountry(val){
     if(val==""){
       const response = await fetch("world.php?");
       if(response.ok){
@@ -37,4 +43,29 @@ window.onload = function(){
         }
       }
     }
+
+    async function fetchCity(val){
+      if(val==""){
+        const response = await fetch("world.php?");
+        if(response.ok){
+          countries = await response.text();
+          console.log("nothing");
+          console.log(countries);
+          result.innerHTML = countries;
+        } else {
+          const message = "An error has occured: ${response.status}";
+          throw new Error(message);
+        }
+      } else{
+          const response = await fetch("world.php?country="+val+"&context=cities");
+          if(response.ok){
+            countries = await response.text();
+            console.log(countries)
+            result.innerHTML = countries;
+          } else {
+            const message = "An error has occured: ${response.status}";
+            throw new Error(message);
+          }
+        }
+      }
 };
--- world.php
+++ world.php
@@ -4,30 +4,57 @@ $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
 
-$search = ucwords($_GET["country"]);
-$search = trim($search);
-$search = stripslashes($search);
+
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$search%'");
+if(isset($_GET)){
+    $country = $_GET['country'];
+    $context =$_GET['context'];
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+if($context=="cities"){
 
+        $stmt = $conn->query("SELECT cities.name, cities.district, cities.population FROM cities JOIN countries ON cities.country_code = countries.code WHERE countries.name LIKE \"%$country%\" ORDER BY countries.name ASC");
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+ }else{
+     $countryres = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+     $count= $countryres->fetchAll(PDO::FETCH_ASSOC);
+ }
+}
 
-
- // if($search !=""){
- //   foreach($result as $res){
- //     if ($search == $res["name"] or $search == $hero["alias"]){
- //       echo(json_encode($hero));
- //       return;
- //     }
- //   }
- //   echo "No match";
- //   return;
- // }
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+<table>
+
+
+       <?php if($context=="cities"): ?>
+
+               <th> Country Name</th>
+               <th> District</th>
+               <th> Population</th>
+
+
+               <?php foreach ($results as $row): ?>
+               <tr>
+                   <td><?= $row['name']; ?></td>
+                   <td><?= $row['district']; ?></td>
+                   <td><?=$row['population']; ?></td>
+               </tr>
+           <?php endforeach; ?>
+
+       <?php else: ?>
+
+               <th> Country Name</th>
+               <th> Continent</th>
+               <th> Independence Year</th>
+               <th> Head of State</th>
+
+           <?php foreach ($count as $row): ?>
+               <tr>
+                   <td><?= $row['name']; ?></td>
+                   <td><?= $row['continent']; ?></td>
+                   <td><?=$row['independence_year']; ?></td>
+                   <td><?= $row['head_of_state']; ?> </td>
+               </tr>
+           <?php endforeach; ?>
+       <?php endif; ?>
+
+</table>
